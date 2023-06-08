a806101edabcf58436ed5c559aac1fd9c9b1f0a1
lahirutech <rmllbrathnayaka@gmail.com>
2023-06-08 17:00:29 +0530

    fix: changing typing speed

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -10,9 +10,9 @@ export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [delta, setDelta] = useState(200 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ['Web Developer', 'Web Designer', 'UI/UX Designer'];
+  const toRotate = ['A Fullstack Developer', 'A Content Creator'];
   const period = 2000;
 
   useEffect(() => {
