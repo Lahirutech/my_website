08a8f4bdc9b948cb9dbbed35261dd1d3dbbe1cc7
lahirutech <rmllbrathnayaka@gmail.com>
2023-06-08 16:36:47 +0530

    fix: minor

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -71,7 +71,7 @@ export const Banner = () => {
                     isVisible ? 'animate__animated animate__fadeIn' : ''
                   }
                 >
-                  <span className='tagline'>Welcome to my Portfolio</span>
+                  <span className='tagline'>Welcome to my Portfolio Bro</span>
                   <h1>
                     {`Hi! I'm Lanka`}{' '}
                     <span
